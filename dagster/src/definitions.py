1d461b2df22819e135343ef6a8f54def13f1b474
Albert Perez <albperez@gbox.adnu.edu.ph>
2024-06-27 14:20:13 +0800

    fix: used os instead of EnvVar

--- dagster/src/definitions.py
+++ dagster/src/definitions.py
@@ -1,3 +1,5 @@
+import os
+
 from src.assets import popular_subreddits, subreddits
 from src.jobs.popular_subreddits import popular_subreddits_job
 from src.resources.db import SQLiteResource
@@ -6,7 +8,7 @@ from src.schedules.daily_popular_subreddits import daily_popular_subreddits_sche
 from src.schedules.daily_subreddits import daily_subreddits_schedule
 from src.sensors.subreddits import adhoc_request_sensor
 
-from dagster import Definitions, EnvVar, load_assets_from_modules
+from dagster import Definitions, load_assets_from_modules
 
 # Asset definitions
 subreddits_asset = load_assets_from_modules(modules=[subreddits])
@@ -23,12 +25,12 @@ all_sensors = [adhoc_request_sensor]
 
 # Resource definitions
 reddit_resource = RedditResource(
-    client_id=EnvVar("REDDIT_CLIENT_ID"),
-    client_secret=EnvVar("REDDIT_CLIENT_SECRET"),
-    user_agent=EnvVar("REDDIT_USER_AGENT"),
+    client_id=os.getenv("REDDIT_CLIENT_ID"),
+    client_secret=os.getenv("REDDIT_CLIENT_SECRET"),
+    user_agent=os.getenv("REDDIT_USER_AGENT"),
 )
 sqlite_resource = SQLiteResource(
-    db_path=EnvVar("SQLITE_DB_PATH"),
+    db_path=os.getenv("SQLITE_DB_PATH"),
 )
 
 # Definitions
