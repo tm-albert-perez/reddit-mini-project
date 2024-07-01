22db2dae49388ba25a485038015c5c0bdbc220c4
Albert Perez <albperez@gbox.adnu.edu.ph>
2024-07-01 14:04:06 +0800

    refactor: load assets using fn

--- dagster/src/jobs/popular_subreddits.py
+++ dagster/src/jobs/popular_subreddits.py
@@ -1,6 +1,10 @@
-from dagster import define_asset_job
+from src.assets import popular_subreddits
+
+from dagster import define_asset_job, load_assets_from_modules
+
+subreddits_asset = load_assets_from_modules(modules=[popular_subreddits])
 
 popular_subreddits_job = define_asset_job(
     name="popular_subreddits_job",
-    selection=["popular_subreddits", "popular_subreddits_posts"],
+    selection=subreddits_asset,
 )
--- dagster/src/jobs/subreddits.py
+++ dagster/src/jobs/subreddits.py
@@ -1,5 +1,9 @@
-from dagster import define_asset_job
+from src.assets import subreddits
+
+from dagster import define_asset_job, load_assets_from_modules
+
+subreddits_asset = load_assets_from_modules(modules=[subreddits])
 
 adhoc_subreddit_request_job = define_asset_job(
-    name="subreddit_job", selection=["subreddit", "subreddit_posts"]
+    name="subreddit_job", selection=subreddits_asset
 )
